f8c6339ce28e644171a755136298edc6fa60c547
Javier Llaca <javier.llaca.o@gmail.com>
2014-04-20 18:46:00 -0400

    Initial Commit

---
+++ Notes/JLOAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  JLOAppDelegate.h
+//  Notes
+//
+//  Created by Javier Llaca on 4/20/14.
+//  Copyright (c) 2014 Llaca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Notes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Notes
+//
+//  Created by Javier Llaca on 4/20/14.
+//  Copyright (c) 2014 Llaca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JLOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JLOAppDelegate class]));
+    }
+}
